1882c74feeb579041d26170d39e8e2278d13d3e9
Connor Sindt <connor-sindt@uiowa.edu>
2023-01-26 18:51:38 -0600

    remove old code

--- lib/CHIP8.cpp
+++ lib/CHIP8.cpp
@@ -1,20 +1,3 @@
 #include "CHIP8.h"
-#include <iostream>
-#include <cstddef>
-#include <bitset>
-#include <SFML/Graphics.hpp>
 
-using namespace std;
 
-class CHIP8{
-    public:
-        CHIP8(){
-            DISP = sf::RenderWindow(sf::VideoMode(64, 32), "CHIP-8");
-            
-        }
-
-    private:
-        char* RAM[4096];  // 4 KB of RAM
-        sf::RenderWindow DISP;  // Display
-        unsigned short int 
-};
