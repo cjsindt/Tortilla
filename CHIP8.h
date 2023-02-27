54441568b05a96e075cfb9318219cb7ec702a4a8
Connor Sindt <connor-sindt@uiowa.edu>
2023-02-27 13:46:46 -0600

    removed old instruction enum

--- CHIP8.h
+++ CHIP8.h
@@ -39,44 +39,6 @@ const static uint8_t FONTSET[FONTSET_SIZE] = {
         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-// List of valid instructions, including NIL for an invalid instruction
-enum Instruction{
-    CLS,
-    RET,
-    SYS_addr,
-    JP_addr,
-    CALL_addr,
-    SE_Vx_byte,
-    SNE_Vx_byte,
-    SE_Vx_Vy,
-    LD_Vx_byte,
-    ADD_Vx_byte,
-    LD_Vx_Vy,
-    OR_Vx_Vy,
-    AND_Vx_Vy,
-    XOR_Vx_Vy,
-    ADD_Vx_Vy,
-    SUB_Vx_Vy,
-    SHR_Vx_Vy,
-    SUBN_Vx_Vy,
-    LD_I_addr,
-    JP_V0_addr,
-    RND_Vx_byte,
-    DRW_Vx_Vy_nibble,
-    SKP_Vx,
-    SKNP_Vx,
-    LD_Vx_DT,
-    LD_Vx_K,
-    LD_DT_Vx,
-    LD_ST_Vx,
-    ADD_I_Vx,
-    LD_F_Vx,
-    LD_B_Vx,
-    LD_I_Vx,
-    LD_Vx_I,
-    NIL
-};
-
 class CHIP8{
     private:
         uint8_t RAM[TOTAL_RAM];     // 4KB of RAM
